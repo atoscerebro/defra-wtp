697f72d4ec267b67f505513d4572d93d55ec5ba6
Abey Onalaja <abey.onalaja@gmail.com>
2022-08-04 14:32:29 +0100

    Add page breadcrumbs and make them slightly dynamic add prenotify page

--- components/page-header/index.tsx
+++ components/page-header/index.tsx
@@ -1,29 +1,25 @@
 import { TopNav } from 'govuk-react';
 
-export const PageHeader  = () => {
-    return <>
-        <TopNav
-            serviceTitle={<TopNav.NavLink href="https://example.com" target="new">Export green list was</TopNav.NavLink>}
-        >
-        <TopNav.NavLink
-            href="https://example.com?q=catdog"
-            target="new"
-        >
-            Navigation item
+export const PageHeader = () => {
+  return (
+    <>
+      <TopNav
+        serviceTitle={
+          <TopNav.NavLink href="https://example.com" target="new">
+            Export green list waste
+          </TopNav.NavLink>
+        }
+      >
+        {/* <TopNav.NavLink href="https://example.com?q=catdog" target="new">
+          Navigation item
         </TopNav.NavLink>
-        <TopNav.NavLink
-            href="https://example.com?q=dogcat"
-            target="new"
-        >
-            Navigation item
+        <TopNav.NavLink href="https://example.com?q=dogcat" target="new">
+          Navigation item
         </TopNav.NavLink>
-        <TopNav.NavLink
-            href="https://example.com?q=dogcat"
-            target="new"
-        >
-            Navigation item
-        </TopNav.NavLink>
-
-        </TopNav>
+        <TopNav.NavLink href="https://example.com?q=dogcat" target="new">
+          Navigation item
+        </TopNav.NavLink> */}
+      </TopNav>
     </>
-}
+  );
+};
---
+++ payloads/page-breadcumbs.ts
@@ -0,0 +1,16 @@
+import { IBreadcrumb } from '../components/page-breadcrumbs';
+
+export const homeCrumbs: IBreadcrumb[] = [
+  { label: 'Waste tracking service ', route: '/' },
+];
+
+export const uniqueRefCrumbs: IBreadcrumb[] = [
+  { label: 'Waste tracking service ', route: '/' },
+  { label: 'Green list waste overview', route: '/' },
+];
+
+export const prenotifyCrumbs: IBreadcrumb[] = [
+  { label: 'Waste tracking service ', route: '/' },
+  { label: 'Green list waste overview', route: '/' },
+  { label: 'Your reference number', route: 'unique-ref' },
+];
