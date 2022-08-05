3598823665d9e54465a364abd0ab48e0209c6549
Patrick Merchant <patrickmerchant9@gmail.com>
2022-08-05 13:33:10 +0100

    Adds summary list component top EWC

--- pages/submit-an-export/about-the-waste/waste-codes-descriptions/ewc-code-summary/index.tsx
+++ pages/submit-an-export/about-the-waste/waste-codes-descriptions/ewc-code-summary/index.tsx
@@ -1,5 +1,14 @@
 import { NextPage } from 'next';
-import { H1, FormGroup, Radio, Paragraph, Caption, Button } from 'govuk-react';
+import {
+  H1,
+  FormGroup,
+  Radio,
+  Paragraph,
+  Caption,
+  Button,
+  Heading,
+  Fieldset,
+} from 'govuk-react';
 import { DisplayInputField } from '../../../../../components/display-input-field';
 import { BaseTaskPage } from '../../../../../components/base-task-page';
 import {
@@ -9,74 +18,82 @@ import {
 import { Form, Field } from 'react-final-form';
 import { useRouter } from 'next/router';
 import { useState } from 'react';
+import { SummaryList } from '../../../../../components/summary-list';
+import { summaryListRows } from '../../../../../payloads/summary-list-rows'; // change import for summary list rows
 
 const EWCCodeSummary: NextPage = () => {
-    const router = useRouter();
-    const [anotherEWCCode, setAnotherEWCCode] = useState(false);
-  
-    return (
-       <>
+  const router = useRouter();
+  const [anotherEWCCode, setAnotherEWCCode] = useState(false);
 
-        {/* Insert summary list component, where rows are EWC codes + change and remove links */}
-        
+  return (
+    <>
+      {/* Insert summary list component, where rows are EWC codes + change and remove links */}
+
       <BaseTaskPage
         backHref="/submit-an-export/about-the-waste/waste-codes-descriptions/ewc-code-radio"
         component={
-          <Form
-            onSubmit={(formObj) => {
-              console.log(formObj);
-              const routerUrl = anotherEWCCode
-                ? '/submit-an-export/about-the-waste/waste-codes-descriptions/ewc-code-summary/ewc-code-input'
-                : '/submit-an-export/about-the-waste/waste-codes-descriptions/national-code-radio';
-              router.push(routerUrl);
-            }}
-          >
-            {({ handleSubmit }) => (
-              <FormGroup>
+          <>
+            <H1 size="LARGE">You have added 3 EWC codes</H1>
+            <SummaryList rows={summaryListRows} />
+
+            <Form
+              onSubmit={(formObj) => {
+                console.log(formObj);
+                const routerUrl = anotherEWCCode
+                  ? '/submit-an-export/about-the-waste/waste-codes-descriptions/ewc-code-summary/ewc-code-input'
+                  : '/submit-an-export/about-the-waste/waste-codes-descriptions/national-code-radio';
+                router.push(routerUrl);
+              }}
+            >
+              {({ handleSubmit }) => (
                 <form onSubmit={handleSubmit}>
-                  <H1 size="MEDIUM">Do you need to add another EWC code?</H1>
-                  <Field name="ewc-code-radio" type="radio" value="yes">
-                    {({ input }) => (
-                      <Radio
-                        {...input}
-                        onClick={() => {
-                          setAnotherEWCCode(true);
-                        }}
-                        inline
+                  <FormGroup>
+                    <H1 size="MEDIUM">Do you need to add another EWC code?</H1>
+                    <Fieldset>
+                      <Field name="ewc-code-radio" type="radio" value="yes">
+                        {({ input }) => (
+                          <Radio
+                            {...input}
+                            onClick={() => {
+                              setAnotherEWCCode(true);
+                            }}
+                            inline
+                          >
+                            Yes
+                          </Radio>
+                        )}
+                      </Field>
+                      <Field
+                        defaultValue="no"
+                        name="ewc-code-radio"
+                        type="radio"
+                        value="no"
                       >
-                        Yes
-                      </Radio>
-                    )}
-                  </Field>
-                  <Field
-                    defaultValue="no"
-                    name="ewc-code-radio"
-                    type="radio"
-                    value="no"
-                  >
-                    {({ input }) => (
-                      <Radio
-                        {...input}
-                        onClick={() => {
-                          setAnotherEWCCode(false);
-                        }}
-                        inline
-                      >
-                        No
-                      </Radio>
-                    )}
-                  </Field>
+                        {({ input }) => (
+                          <Radio
+                            {...input}
+                            onClick={() => {
+                              setAnotherEWCCode(false);
+                            }}
+                            inline
+                          >
+                            No
+                          </Radio>
+                        )}
+                      </Field>
+                    </Fieldset>
+                  </FormGroup>
                   <ButtonWrapper>
                     <Button type="submit">Save and continue</Button>
                   </ButtonWrapper>
                 </form>
-              </FormGroup>
-            )}
-          </Form>
+              )}
+            </Form>
+          </>
         }
       ></BaseTaskPage>
-      </> 
-    );
-}
+    </>
+  );
+};
 
 export default EWCCodeSummary;
