f0f31be2cbce2ea702156fb5031ac15094d3e56f
Patrick Merchant <patrickmerchant9@gmail.com>
2022-08-09 09:43:05 +0100

    fixes display input field styling

--- components/display-input-field/styled-components.ts
+++ components/display-input-field/styled-components.ts
@@ -1,9 +1,14 @@
 import styled from 'styled-components';
+import { SPACING, MEDIA_QUERIES } from '@govuk-react/constants';
 
 export const DisplayInputFieldWrapper = styled.div`
+  margin-bottom: ${SPACING.SCALE_3};
   margin-left: 18px;
-  margin-bottom: 1rem;
-  padding-left: 1rem;
+  padding-left: 33px;
   border-left: 4px solid #b1b4b6;
   color: #b1b4b6;
+
+  ${MEDIA_QUERIES.TABLET} {
+    margin-bottom: ${SPACING.SCALE_4};
+  }
 `;
