e4219f7cd9f43210449c05ba9cfbb5b60c284ac4
Lawrence Witt <55201111+lawrence-witt@users.noreply.github.com>
2022-08-09 17:54:36 +0100

    DEF-9: Configure next.js pages extensions (#29)
    * Add pageExtensions config and rename files
    
    * Rename page files
    
    * Fix ariaId on radio conditionals

--- pages/submit-an-export/index.tsx
+++
@@ -1,9 +0,0 @@
-import { NextPage } from 'next';
-// import { UniqueRefForm } from '../../components/form-pages/unique-ref-form';
-import { Main } from 'govuk-react';
-
-const SubmitExport: NextPage = () => {
-  return <Main>{/* <UniqueRefForm /> */}</Main>;
-};
-
-export default SubmitExport;
